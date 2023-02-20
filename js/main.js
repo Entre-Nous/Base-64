5dea2ecb493cd33b8300227501b2ea4483a7f3b7
Nassim <akou.nassim@gmail.com>
2023-02-20 13:03:09 +0100

    add action of button show and hide menu

--- js/main.js
+++ js/main.js
@@ -1,30 +1,10 @@
 if (document.title === "Encode-Decode Base-64") {
-  // les elements de notre page index.html
-  var headerText = document.querySelector(".title-index");
-  let descripton = document.getElementById("desc");
-  let buttons = document.getElementsByClassName("btn");
-  let text =
-    "Notre outil en ligne facilite l'encodage du texte en base64 ou le décodage de base64 en texte.";
-  let letters = text.split("");
+  let btnShow = document.querySelector(".fa-bars");
+  let menu = document.querySelector(".links");
 
-  // l'animation de text
-  let i = 0;
-  setInterval(function () {
-    if (i === letters.length) {
-      clearInterval(this);
-    } else {
-      descripton.textContent += letters[i];
-      i++;
-    }
-  }, 50);
-
-  // si la page est chargée je fais quelque animation sur les composants
-  // window.onload = function ()
-
-  headerText.style.opacity = "1";
-  headerText.style.paddingTop = "30vh";
-  descripton.style.opacity = "1";
-  buttons[0].style.opacity = "1";
+  btnShow.onclick = () => {
+    menu.classList.toggle("show-menu");
+  };
 } else if (document.title === "Encode-Base64") {
   // la page d'encodage
 
